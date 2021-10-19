fd19c53ca505f057d362a953c30a7ee64eb03016
ivekw <petrovic1986@gmail.com>
2021-10-19 16:47:41 +0200

    userRoutesProblem

--- client/src/requestMetods.js
+++ client/src/requestMetods.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 const BASE_URL = "https://etiktakpizza.herokuapp.com/";
-const TOKEN =
+/* const TOKEN =
   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
-    .accessToken || "";
+    .accessToken || ""; */
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
 
-export const userRequest = axios.create({
+/* export const userRequest = axios.create({
   baseURL: BASE_URL,
   header: { token: `Bearer ${TOKEN}` },
-});
+}); */
