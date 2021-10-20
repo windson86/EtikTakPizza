ce70b9349d5e0f38a66b110c00be3d6f67b305c5
ivekw <petrovic1986@gmail.com>
2021-10-20 02:54:01 +0200

    baseUrlError

--- client/src/requestMetods.js
+++ client/src/requestMetods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/";
+const BASE_URL = "https://etiktakpizza.herokuapp.com/";
 const TOKEN = localStorage.getItem("accessToken");
 
 export const publicRequest = axios.create({
