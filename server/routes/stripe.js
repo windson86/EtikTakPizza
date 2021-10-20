8fc989bf3d47e9741b62ed67473c7c90b60ec6b5
ivekw <petrovic1986@gmail.com>
2021-10-20 19:53:23 +0200

    stripeCheckout

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -3,7 +3,6 @@ const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_KEY);
 
 router.post("/payment", async (req, res) => {
-  console.log(req.body);
   try {
     stripe.charges.create(
       {
@@ -13,7 +12,6 @@ router.post("/payment", async (req, res) => {
       },
       (stripeErr, stripeRes) => {
         if (stripeErr) {
-          console.log(stripeErr);
           res.status(500).json(stripeErr);
         } else {
           res.status(200).json(stripeRes);
