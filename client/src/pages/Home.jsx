1b452bf610d2cdb6a56d6d5789d067970481740c
ivekw <petrovic1986@gmail.com>
2021-10-23 19:09:06 +0200

    mobileSetupReduxProblems

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,16 +3,11 @@ import Navbar from '../components/Navbar'
 import OnSale from '../components/OnSale'
 import Slider from '../components/Slider'
 import Footer from '../components/Footer'
-/* import { useDispatch, useSelector } from 'react-redux'
-import { getFavoritePizzas } from '../redux/ApiCalls' */
+
 
 
 const Home = () => {
-   /*  const dispatch = useDispatch();
-    const pizzas = useSelector((state) => state.pizzas.pizzas);
-    useEffect(() => {
-        getFavoritePizzas(dispatch)
-       }, [dispatch]); */
+
     
     return (
         <div>
